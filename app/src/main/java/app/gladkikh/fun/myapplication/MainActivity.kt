b80ec286d53f979d1136ad278a486b3dc8c04086
ZavbakGit <ag@799000.ru>
2019-06-12 18:39:54 +1000

    Initial commit

---
+++ app/src/main/java/app/gladkikh/fun/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.gladkikh.`fun`.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
